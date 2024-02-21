ecb134b77989a860f814af7ef278d4906b4617ec
NyaShinn1204 <team.nyarara@gmail.com>
2024-02-21 19:33:06 +0900

    Add Sample Gui

---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+import time
+import json
+import threading
+import webbrowser
+import subprocess
+import requests
+import colorama
+import tkinter as tk
+import customtkinter as ctk
+from PIL import Image
+from pystyle import *
+from colorama import Fore
+from customtkinter import *
+from CTkMessagebox import CTkMessagebox
+from CTkToolTip import *
+
+version = "0.1.0"
+
+def printl(num, data):
+  filename = __file__
+  if num == "error":
+    print(f"[{Fore.LIGHTRED_EX}Error{Fore.RESET}] [{filename}] " + data)
+  if num == "debug":
+    print(f"[{Fore.LIGHTCYAN_EX}Debug{Fore.RESET}] [{filename}] " + data)
+  if num == "info":
+    print(f"[{Fore.LIGHTGREEN_EX}Info{Fore.RESET}] [{filename}] " + data)
+    
+def get_hwid():
+  try:
+    if os.name == 'posix':
+      cmd = 'cat /etc/machine-id'
+      uuid = subprocess.check_output(cmd,shell=True)
+      uuid = uuid[:-1].decode('utf-8')
+      return uuid
+    if os.name == 'nt':
+      cmd = 'powershell -Command (Get-WmiObject -Class Win32_ComputerSystemProduct).UUID'
+      uuid = subprocess.run(cmd, capture_output=True, text=True, shell=True)
+      uuid = uuid.stdout.strip()
+      return uuid
+  except:
+    printl("error", "get_hwid exception error wrong")
+
+# Load First
+logo = f"""
+                    ___  ____     _       _       _     _    ______      _     _           
+    ..&@.           |  \/  (_)   | |     (_)     | |   | |   | ___ \    (_)   | |          
+  ..@@@&.           | .  . |_  __| |_ __  _  __ _| |__ | |_  | |_/ /__ _ _  __| | ___ _ __ 
+  .&&&&&,..         | |\/| | |/ _` | '_ \| |/ _` | '_ \| __| |    // _` | |/ _` |/ _ \ '__|
+  ..&&&&&&&#.       | |  | | | (_| | | | | | (_| | | | | |_  | |\ \ (_| | | (_| |  __/ |   
+    ..#&&&...       \_|  |_/_|\__,_|_| |_|_|\__, |_| |_|\__| \_| \_\__,_|_|\__,_|\___|_|   
+                             __/ |                                         
+                            |___/                                      
+"""
+print(Colorate.Horizontal(Colors.white_to_blue, logo, 1))
+print(f"""
+You HWID: [{get_hwid()}]                Version: [{version}]
+-----------------------"""
+)
