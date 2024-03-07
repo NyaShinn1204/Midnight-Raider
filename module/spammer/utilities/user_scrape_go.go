05f9a13bfc337a1a370aa19f4c66c79af56ac08e
NyaShinn1204 <team.nyarara@gmail.com>
2024-03-07 21:38:17 +0900

    Fix User_scraper Go

--- module/spammer/utilities/user_scrape_go.go
+++ module/spammer/utilities/user_scrape_go.go
@@ -30,7 +30,17 @@ func main() {
 	fmt.Printf("Server ID: %s\n", serverID)
 	fmt.Printf("Channel ID: %s\n", channelID)
 	fmt.Printf("Token: %s\n", token)
-
+	response := checktoken(token)
+	if response == 200 {
+		log.Printf("[VALID]   %v [%v]\n", token, response)
+	}
+	if response == 403 {
+		log.Printf("[LOCKED]  %v [%v]\n", token, response)
+		os.Exit(1)
+	} else {
+		log.Printf("[INVALID] %v [%v]", token, response)
+		os.Exit(1)
+	}
 	fmt.Printf("Scraping in %s with %s\n", serverID, token)
 	members := scrape(token, serverID, channelID)
 	fmt.Printf("Total Scrapped: %d\n", len(members))
@@ -40,6 +50,39 @@ func main() {
 	}
 
 	fmt.Printf("Scrapped Id: %v\n", userIDs)
+}
+
+func checktoken(token string) int {
+	url := "https://discord.com/api/v9/users/@me/affinities/guilds"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Printf("%v", err)
+		return -1
+	}
+	req.Header.Set("authorization", token)
+	httpClient := &http.Client{}
+	resp, err := httpClient.Do(commonheaders(req))
+	if err != nil {
+		log.Printf("%v", err)
+		return -1
+	}
+	return resp.StatusCode
+}
+
+func commonheaders(req *http.Request) *http.Request {
+	req.Header.Set("x-super-properties", "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRmlyZWZveCIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJlbi1VUyIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2OjkzLjApIEdlY2tvLzIwMTAwMTAxIEZpcmVmb3gvOTMuMCIsImJyb3dzZXJfdmVyc2lvbiI6IjkzLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTAwODA0LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==")
+	req.Header.Set("sec-fetch-dest", "empty")
+	req.Header.Set("sec-fetch-mode", "cors")
+	req.Header.Set("sec-fetch-site", "same-origin")
+	req.Header.Set("x-context-properties", "eyJsb2NhdGlvbiI6IkpvaW4gR3VpbGQiLCJsb2NhdGlvbl9ndWlsZF9pZCI6Ijg4NTkwNzE3MjMwNTgwOTUxOSIsImxvY2F0aW9uX2NoYW5uZWxfaWQiOiI4ODU5MDcxNzIzMDU4MDk1MjUiLCJsb2NhdGlvbl9jaGFubmVsX3R5cGUiOjB9")
+	req.Header.Set("sec-ch-ua", "'Chromium';v='92', ' Not A;Brand';v='99', 'Google Chrome';v='92'")
+	req.Header.Set("accept", "*/*")
+	req.Header.Set("accept-language", "en-GB")
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("sec-ch-ua-platform", "\"Windows\"")
+	req.Header.Set("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.16 Chrome/91.0.4472.164 Electron/13.4.0 Safari/537.36")
+	return req
 }
 
 func rangeCorrector(ranges [][]int) [][]int {
@@ -173,6 +216,12 @@ func (ds *DiscordSocket) sockMessage(message []byte) {
 					"member_count": guild.(map[string]interface{})["member_count"].(float64),
 				}
 				log.Println("Added Guild ID to ds.Guilds:", guildID)
+				guild, ok := ds.Guilds[guildID]
+				if !ok || guild == nil {
+					log.Printf("Error: Guild not found for ID %s or it is nil\n", guildID)
+					os.Exit(1)
+					//return
+				}
 			}
 		case "READY_SUPPLEMENTAL":
 			if decoded["t"].(string) == "READY_SUPPLEMENTAL" {
@@ -188,6 +237,7 @@ func (ds *DiscordSocket) sockMessage(message []byte) {
 				guild, ok := ds.Guilds[guildID]
 				if !ok || guild == nil {
 					log.Printf("Error: Guild not found for ID %s or it is nil\n", guildID)
+					//os.Exit(1)
 					//return
 				}
 
@@ -293,14 +343,6 @@ func containsType(types []interface{}, target string) bool {
 		}
 	}
 	return false
-}
-
-func toJSON(data interface{}) string {
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return string(jsonData)
 }
 
 func (ds *DiscordSocket) runForever() {
